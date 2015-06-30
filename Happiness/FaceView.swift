0c5fcf1f0d9ccccb6feb714e069d419423b60e08
vinayaks101 <vinayak.s101@gmail.com>
2015-06-30 19:06:12 +0530

    Initial commit with the Face being drawn using UIView

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,33 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Vinayak Srivastava on 30/06/15.
+//  Copyright (c) 2015 Vinayak Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    
+    var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay() } }
+    var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay() } }
+    var scale: CGFloat = 0.90 { didSet { setNeedsDisplay() } }
+    
+    var faceCenter:  CGPoint {
+        return convertPoint(center, fromView: superview)
+    }
+    
+    var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2 * M_PI), clockwise: true)
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
+    }
+    
+
+}
